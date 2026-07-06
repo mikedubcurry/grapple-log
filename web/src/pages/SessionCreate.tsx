b6877c631c7af0c9a633307638c0603992d8203b
mikedubcurry <43300851+mikedubcurry@users.noreply.github.com>
2026-07-05 20:25:02 -0400

    text area component for notes and injury explaination

--- web/src/pages/SessionCreate.tsx
+++ web/src/pages/SessionCreate.tsx
@@ -12,6 +12,8 @@ export const SessionCreate = () => {
     const [art, setArt] = useState('');
     const [tech, setTech] = useState<string[]>([]);
     const [injured, setInjured] = useState<boolean | null>(null);
+    const [injExplain, setInjExplain] = useState('');
+    const [notes, setNotes] = useState('');
     return (
         <main className="mx-2 pt-2">
             <div className="py-4">
@@ -21,7 +23,7 @@ export const SessionCreate = () => {
                 <MultiComboInput label="Tech" state={[tech, setTech]} />
             )}
             {tech.length > 0 && (
-                <>
+                <div className="mb-2">
                     <p className="">Injured?</p>
                     <div className="flex flex-col">
                         <div className="flex gap-2">
@@ -33,16 +35,56 @@ export const SessionCreate = () => {
                             <label htmlFor="no">No</label>
                         </div>
                     </div>
-                    {/* TODO: add injury details */}
-                </>
+                    {injured && (
+                        <TextBlock
+                            value={injExplain}
+                            onChange={setInjExplain}
+                            placeholder="Explain"
+                        />
+                    )}
+                </div>
             )}
             {injured !== null && (
-                <>
-                    <p>Notes</p>
-                    {/* TODO: add notes section */}
-                </>
-
+                <TextBlock
+                    value={notes}
+                    onChange={setNotes}
+                    placeholder="Notes"
+                />
             )}
         </main>
+    )
+}
+
+export interface TextBlockProps {
+    value: string;
+    onChange: (value: string) => void;
+    placeholder: string;
+}
+
+const TextBlock = ({ value, onChange, placeholder }: TextBlockProps) => {
+    return (
+        <div className="relative w-full ">
+            <textarea
+                className="field-sizing-content outline-none w-full resize-none bg-transparent font-mono text-sm p-2"
+                placeholder={placeholder}
+                spellCheck={false}
+                value={value}
+                onChange={e => onChange(e.target.value)}
+                id="notes"
+            />
+            <svg
+                viewBox="0 0 100 100"
+                className="absolute inset-0 w-full h-full pointer-events-none"
+                preserveAspectRatio="none"
+            >
+                <rect
+                    x="0.5" y="0.5" width="99" height="99"
+                    fill="none"
+                    stroke="#000"
+                    strokeDasharray={"3 3"}
+                    vectorEffect="non-scaling-stroke"
+                />
+            </svg>
+        </div>
     )
 }
